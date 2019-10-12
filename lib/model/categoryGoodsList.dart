7e30642737acb1580de3d3fcf8183d1499d754f7
itxing <itxing666@163.com>
2019-10-12 09:32:11 +0800

    暂存

---
+++ lib/model/categoryGoodsList.dart
@@ -0,0 +1,11 @@
+// import 'category.dart';
+
+// class CategoryBigListModel {
+//   List<CategoryBigModel> data;
+//   CategoryBigListModel(this.data);
+//   factory CategoryBigListModel.fromJson(List json) {
+//     return CategoryBigListModel(
+//       json.map((i)=>CategoryBigModel.fromJson((i))).toList()
+//     );
+//   }
+// }
